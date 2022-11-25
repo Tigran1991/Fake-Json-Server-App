65cc679e089b0f829786836295496fff230ed6b9
Tigran Aginyan <taginyan91@gmail.comm>
2022-11-26 00:50:39 +0400

    fix adding and deleting items using json-server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import "./App.css"
 
 function App() {
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [age, setAge] = useState()
+  const [personData, setPersonData] = useState([])
+  const [stateChangeIndicator, setStateChangeIndicator] = useState(false)
+
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value)
+  }
+
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value)
+  }
+
+  const handleAgeChange = (e) => {
+    setAge(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then((response) => response.json())
+    setStateChangeIndicator(!stateChangeIndicator)
+  }
+
+  const handleClick = (e) => {
+    const id = e.target.id
+    fetch(`http://localhost:5000/users/${id}`, { method: "DELETE" })
+    setStateChangeIndicator(!stateChangeIndicator)
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setPersonData(data)
+      })
+  }, [stateChangeIndicator])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="FirstName"
+          onChange={handleFirstNameChange}
+        />
+        <input
+          type="text"
+          placeholder="LastName"
+          onChange={handleLastNameChange}
+        />
+        <input type="number" placeholder="Age" onChange={handleAgeChange} />
+        <button type="submit">Submit</button>
+      </form>
+      {personData &&
+        personData.map((person) => (
+          <>
+            <ul key={person.id} onClick={handleClick} id={person.id}>
+              {person.firstName}
+            </ul>
+          </>
+        ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
